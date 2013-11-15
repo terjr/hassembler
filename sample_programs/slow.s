7510cbcfb4c0d7a85cd812932b2dc8661b9f1bdc
Terje Runde <terje.runde@gmail.com>
2013-11-15 14:40:22 +0100

    Evolving sample program: slow.

--- sample_programs/slow.s
+++ sample_programs/slow.s
@@ -1,5 +1,7 @@
-lda r1, [r0]
-nop
-stb r1, [r0]
-ldi #1
-stb r1, [r31]
+lda r2, [r0]    ; load newest sample from A, i.e. A[0]
+nop             ; wait for completion
+stb r2, [r0]    ; store the sample unaltered to B[0]
+ldi #1          ; prepare a #1 to address B[1]
+stb r2, [r1]    ; store the same sample to B[1]
+nop             ; wait for load to happen
+halt            ; die
